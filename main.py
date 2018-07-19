c0c6676ce5ecd97ffaecad254efafd543c717411
Polloz <30330004+LilChickenz@users.noreply.github.com>
2018-07-19 15:25:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-q=['hi']
-a=['hello']
+import random
+q=['hi', 'how are you']
+a=['hello', 'good']
 num=()
 say='hi'
 turn=2
-print('hi')
+print('q: hi')
 a.append(input('a: '))
 while True:
   if turn % 2 == 0:
     response=input('q: ')
     if response in q:
-      print(a[q.index(response)])
+      print('a: ',a[q.index(response)])
     else:
       q.append(response)
-      print('?')
+      print('a: ?')
   else:
     num=random.randint(0, len(q)-1)
-    print(q[num])
+    print('q: ',q[num])
     try:
       tst=input('a: ')
       a[num]=tst
